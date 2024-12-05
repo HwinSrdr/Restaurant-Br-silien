1d2b553b38b35a654aac872f2e1baaf5dc366b1d
HwinSrdr <sriash168@gmail.com>
2024-12-05 14:20:45 +0100

    update

--- script.js
+++ script.js
@@ -7,25 +7,84 @@
 
 
 
-
 fetch(`data.json`)
 
 .then((rep) => {
-    // Javascript recoit une réponse de l'api(objet js pas exploitable directement => il faut le transformer en json)
+    
     return rep.json()
 })
     .then(donnee => {
-        //Ensuite j'ai accès ici à ma donnée
+        //j'ai accès ici à ma donnée
         console.log(donnee)
+
+        // j'affiche le nom commercial : 
+        //document.querySelector('h1').innerHTML = donnee.nomCommercial
         //Ici j'ai une liste de produit
         //Je boucle sur le tableau de données:
-        donnee.forEach(recette => {
-            /* prod.ingredients.forEach(ingredient => {
-                let typeIngredients = ingredient.aliment
-                ingredient(typeIngredients)
-                });*/
-                
-                affiche(recette)
-            });
+
+
+        donnee.plats.forEach(plat=>{
+            afficherUnPlat(plat)
         })
 
+    })
+    function affiche (desc) {
+
+
+
+    // SECTION HERO
+
+let hero = document.querySelector ("#hero")
+hero.innerHTML +=` 
+div class="container flex spaceBetween alignCenter center">
+                <div>
+                    <h1> ${donnee.NomCommercial} </h1>
+                    <p> ${donnee.texteAccroche} </p>
+                    <a href=""> ${donnee.texteBouton} </a>
+                    <div>
+                        <ul class="flex">
+                            <li><a href="">Facebook</a></li>
+                            <li><a href="">Twitter</a></li>
+                            <li><a href="">Instagram</a></li>
+                            <li><a href="">TikTok</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <img src="assets/plat-abdoul.png" alt="">
+                </div>
+            </div>`;
+
+
+            // SECTION PROMESSE
+            let listePro = "";
+            desc.promessesClients.forEach(promesse=>{
+                let sectionPromesse = document.querySelector ("#promesse")
+                sectionPromesse.innerHTML += 
+                listePro+= `<p> ${promesse} </p> `
+                
+            })
+            console.log(listeIng)
+
+
+            // SECTION MENUS
+            function afficherUnPlat (plat) {
+            let listeMenu ="";
+            desc.plats.forEach(plat => {
+                listeMenu += `
+                <div class="flex alignCenter spaceEvenly menu"> <!-- 1 Menu -->
+                    <div class="w40">
+                        <h3> ${plat.nom} </h3>
+                        <p> ${plat.desc} </p>
+                    </div>
+                    <img src=" ${plat.imageUrl} " alt="">
+                </div> `
+                
+            });
+            console.log(listeMenu)
+
+
+
+}
+
+    }
